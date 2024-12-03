75b01b63d37cf10920f0b1a3e70ff6cc1d42c3d1
ivankdev-fullstack <ivan.kononenko.dev@gmail.com>
2024-12-03 20:05:26 +0200

    feat: implement route error handling

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 import Home from "./ui/Home";
 
 const router = createBrowserRouter([
@@ -14,7 +15,12 @@ const router = createBrowserRouter([
     element: <AppLayout />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/menu", element: <Menu />, loader: menuLoader },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
       { path: "/cart", element: <Cart /> },
       { path: "/order/:id", element: <Order /> },
       { path: "/order/new", element: <CreateOrder /> },
