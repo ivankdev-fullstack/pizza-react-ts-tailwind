444b18cd6c12b3673afe583f6318c7ea7dbc280e
ivankdev-fullstack <ivan.kononenko.dev@gmail.com>
2024-12-05 00:23:17 +0200

    feat: add cart overview

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,25 +1,27 @@
-import React from "react";
 import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
 import Loader from "./Loader";
 
-const AppLayout = () => {
+function AppLayout() {
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
+
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
 
       <Header />
 
-      <main>
-        <Outlet />
-      </main>
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
   );
-};
+}
 
 export default AppLayout;
